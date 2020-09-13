8aa81d13cc61779f0dab58877e4776797e512d2f
Pulkit Verma <vpulkit567@gmail.com>
2020-09-13 21:06:45 +0530

    Started Front End

---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles.css"
+
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-white">This is Home</h1>
+        </div>
+    );
+}
